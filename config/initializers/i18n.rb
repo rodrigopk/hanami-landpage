614170a4afae546168eaa72c6d470d1a43bc0a03
rodrigopk <rodrigopk@gmail.com>
2019-02-10 09:57:08 +0100

    Fix translations

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -8,23 +8,24 @@ I18n.load_path << Dir[Hanami.root.join('config/locales/**/*.yml')]
 
 I18n.available_locales = %i[
   zu
-  de de-de de-ch de-at de-DE de-CH de-AT
-  en en-us en-gb en-US en-GB
-  pt-BR pt-br pt
+  de de-DE de-CH de-AT
+  en en-US en-GB en-DE
+  pt pt-br pt-BR pt-PT
 ]
 
 I18n.default_locale = 'zu'
 
-I18n.fallbacks.map(pt: :"pt-BR")
-I18n.fallbacks.map(de: :en)
-# rubocop:disable Style/HashSyntax
-I18n.fallbacks.map(:"de-DE" => :de)
-I18n.fallbacks.map(:"de-AT" => :de)
-I18n.fallbacks.map(:"de-CH" => :de)
+I18n.fallbacks.map('pt' => 'en-US')
+I18n.fallbacks.map('pt-BR' => 'pt')
+I18n.fallbacks.map('pt-PT' => 'pt-BR')
+I18n.fallbacks.map('de' => 'en-US')
+I18n.fallbacks.map('de-DE' => 'de')
+I18n.fallbacks.map('de-AT' => 'de')
+I18n.fallbacks.map('de-CH' => 'de')
 
-I18n.fallbacks.map(:"pt-BR" => :en)
-I18n.fallbacks.map(:"en-US" => :en)
-I18n.fallbacks.map(:"en-GB" => :en)
-# rubocop:enable Style/HashSyntax
+I18n.fallbacks.map('en-US' => 'en')
+I18n.fallbacks.map('en-GB' => 'en')
+
+I18n.fallbacks.map('en' => '')
 
 I18n.backend.load_translations
--- lib/landpage/helpers/locale.rb
+++ lib/landpage/helpers/locale.rb
@@ -7,7 +7,27 @@ module Landpage
     end
 
     def set_locale
-      I18n.locale = request.env['HTTP_ACCEPT_LANGUAGE'] || I18n.default_locale
+      I18n.locale = locale_from_request_or_default_locale
+    end
+
+    private
+
+    def locale_from_request_or_default_locale
+      locale = locale_from_request
+      return locale if included_in_available_translations?(locale)
+
+      I18n.default_locale
+    end
+
+    def locale_from_request
+      languages = request.accept_language
+      return unless languages
+
+      !languages.empty? && languages[0][0].to_sym
+    end
+
+    def included_in_available_translations?(locale)
+      I18n.available_locales.include?(locale)
     end
   end
 end
